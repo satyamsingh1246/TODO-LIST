1764471716453179f754fec26cc2e244541d5e88
Satyam Singh <5801satyam@gmail.com>
2024-12-30 23:48:07 +0530

    first commit

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+   <h1 className='text-7xl underline font-semibold justify-center flex my-4 '>Todo-List</h1>
+  )
+}
+
+export default Navbar
